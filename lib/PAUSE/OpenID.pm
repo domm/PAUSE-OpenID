0f348867e502f1b52b26dd2ccbce76f767aac177
Thomas Klausner <domm@cpan.org>
2008-11-28 08:48:15 +0100

    create repo skeleton using Module::Setup
    * module-setup --flavour domm PAUSE::OpenID

---
+++ lib/PAUSE/OpenID.pm
@@ -0,0 +1,34 @@
+package PAUSE::OpenID;
+
+use strict;
+use warnings;
+use 5.010;
+use version; our $VERSION = version->new('0.01');
+
+1;
+__END__
+
+=head1 NAME
+
+PAUSE::OpenID -
+
+=head1 SYNOPSIS
+
+  use PAUSE::OpenID;
+
+=head1 DESCRIPTION
+
+PAUSE::OpenID is
+
+=head1 AUTHOR
+
+Thomas Klausner E<lt>domm {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
