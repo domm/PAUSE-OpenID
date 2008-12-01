555a7498a4e1b46ab8019480205471f595ed5e35
Thomas Klausner <domm@cpan.org>
2008-12-01 23:23:13 +0100

    corrected memcached config

--- pause_openid.pl
+++ pause_openid.pl
@@ -11,7 +11,7 @@ return {
         flash_to_stash => 1,
         expires        => 172800,    # two days
         memcached_new_args => {
-            data => [ "10.10.10.108:11211" ],
+            data => [ "localhost:11211" ],
             namespace => "pause_openid_session",
         },
     },
